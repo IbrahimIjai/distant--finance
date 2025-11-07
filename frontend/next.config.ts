840a1769ed527b4549176ea7b5fb368b43e5abef
ibrahimsunday <ibrahimsundayijai@gmail.com>
2025-11-07 20:34:57 +0100

    updated next config for next 16

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	webpack: (config) => {
-		config.externals.push("pino-pretty", "lokijs", "encoding");
-		return config;
-	},
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	/* config options here */
+	// cacheComponents: true,
 	images: {
 		domains: [
 			"ipfsfiles.distant.finance",
